c25f99f38e6ee8bc617bb8ed1ae3c5a15abceec2
Andrew Ratnikov <ratnikov.am@gmail.com>
2017-09-29 22:22:39 +0300

    Delete addLabels.js

--- util/addLabels.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/cermin', { useMongoClient: true, promiseLibrary: global.Promise });
-
-const Label = require('../model/label');
-
-const test1 = new Label({ label: 'test1' });
-
-test1.save((err) => { err ? console.log(err) : console.log('added') });
